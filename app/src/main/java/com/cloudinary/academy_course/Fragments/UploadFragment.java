ced0061a62a89350352701d86b2b1c442fbd1c9d
jenbrissman <brissman514@gmail.com>
2023-07-18 12:46:28 +0100

    adding upload capability

--- app/src/main/java/com/cloudinary/academy_course/Fragments/UploadFragment.java
+++ app/src/main/java/com/cloudinary/academy_course/Fragments/UploadFragment.java
@@ -1,15 +1,25 @@
 package com.cloudinary.academy_course.Fragments;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
+import com.cloudinary.Transformation;
 import com.cloudinary.academy_course.databinding.UploadFragmentBinding;
 import com.cloudinary.academy_course.databinding.WelcomeFragmentBinding;
+import com.cloudinary.android.MediaManager;
+import com.cloudinary.android.callback.ErrorInfo;
+import com.cloudinary.android.callback.UploadCallback;
+
+import java.util.Map;
 
 public class UploadFragment extends Fragment {
 
@@ -28,6 +38,44 @@ public class UploadFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        
+        uploadImage();
+    }
+
+    private void uploadImage() {
+        Uri fileUri = Uri.parse("android.resource://"+getActivity().getPackageName()+"/drawable/butterfly");
+        String requestId = MediaManager.get().upload(fileUri)
+                .unsigned("unsigned-image")
+                .callback(new UploadCallback() {
+                    @Override
+                    public void onStart(String requestId) {
+                        Log.d("Academy Course", "starting upload!!");
+                    }
+
+                    @Override
+                    public void onProgress(String requestId, long bytes, long totalBytes) {
+                        Log.d("Academy Course", "upload in progress");
+                    }
+
+                    @Override
+                    public void onSuccess(String requestId, Map resultData) {
+                        String publicId = (String) resultData.get("public_id");
+                        Log.d("Academy Course", "completed!!");
+                        setImageView(publicId);
+                    }
+
+                    @Override
+                    public void onError(String requestId, ErrorInfo error) {
+                        Log.d("Academy Course", "there has been an error");
+                    }
+
+                    @Override
+                    public void onReschedule(String requestId, ErrorInfo error) {
+
+                    }
+                })
+                .dispatch();
+
     }
 
     @Override
@@ -36,4 +84,14 @@ public class UploadFragment extends Fragment {
         binding = null;
     }
 
+
+    private void setImageView(String publicId) {
+        String URL = MediaManager.get().url().generate(publicId);
+        ImageView uploadImageview = binding.uploadImageview;
+        Glide.with(this).load(URL).into(uploadImageview);
+    }
+
+
 }
+
+
