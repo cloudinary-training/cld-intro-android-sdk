55e0f8049c7c0949d949924ba5c2adb79170d059
jenbrissman <brissman514@gmail.com>
2023-08-06 23:12:35 -0700

    attempting large upload

--- app/src/main/java/com/cloudinary/academy_course/Fragments/UploadLargeFragment.java
+++ app/src/main/java/com/cloudinary/academy_course/Fragments/UploadLargeFragment.java
@@ -1,6 +1,8 @@
 package com.cloudinary.academy_course.Fragments;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +11,11 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.cloudinary.academy_course.databinding.UploadLargeFragmentBinding;
+import com.cloudinary.android.MediaManager;
+import com.cloudinary.android.callback.ErrorInfo;
+import com.cloudinary.android.callback.UploadCallback;
+
+import java.util.Map;
 
 public class UploadLargeFragment extends Fragment {
 
@@ -27,6 +34,45 @@ public class UploadLargeFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        
+        uploadLargeImage();
+    }
+
+
+    private void uploadLargeImage() {
+        Uri fileUri = Uri.parse("android.resource://"+getActivity().getPackageName()+"/drawable/green");
+        String requestId = MediaManager.get().upload(fileUri)
+                .unsigned("unsigned-image")
+                .callback(new UploadCallback() {
+                    @Override
+                    public void onStart(String requestId) {
+                        Log.d("Academy Course", "starting large upload!!");
+                    }
+
+                    @Override
+                    public void onProgress(String requestId, long bytes, long totalBytes) {
+                        Log.d("Academy Course", "large upload in progress");
+                    }
+
+                    @Override
+                    public void onSuccess(String requestId, Map resultData) {
+                        String publicId = (String) resultData.get("public_id");
+                        Log.d("Academy Course", "large upload completed!!");
+                        setImageView(publicId);
+                    }
+
+                    @Override
+                    public void onError(String requestId, ErrorInfo error) {
+                        Log.d("Academy Course", "there has been an error");
+                    }
+
+                    @Override
+                    public void onReschedule(String requestId, ErrorInfo error) {
+
+                    }
+                })
+                .dispatch();
+
     }
 
     @Override
@@ -35,4 +81,11 @@ public class UploadLargeFragment extends Fragment {
         binding = null;
     }
 
+    public void setImageView(String imageView) {
+        this.imageView = imageView;
+    }
+
+    public String getImageView() {
+        return imageView;
+    }
 }
