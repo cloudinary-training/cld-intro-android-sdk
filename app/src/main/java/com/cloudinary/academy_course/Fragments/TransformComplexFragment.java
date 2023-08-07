2239105eee24ab1cef680f1331e3aca01abaf966
jenbrissman <brissman514@gmail.com>
2023-08-06 22:30:38 -0700

    adding complex transform, adding large preprocessing example 30mb image

--- app/src/main/java/com/cloudinary/academy_course/Fragments/TransformComplexFragment.java
+++ app/src/main/java/com/cloudinary/academy_course/Fragments/TransformComplexFragment.java
@@ -4,11 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
+import com.cloudinary.Transformation;
 import com.cloudinary.academy_course.databinding.TransformComplexFragmentBinding;
+import com.cloudinary.android.MediaManager;
 
 public class TransformComplexFragment extends Fragment {
 
@@ -27,12 +31,24 @@ public class TransformComplexFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        setImageView("cheetah");
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+
+    private void setImageView(String publicId) {
+        String URL = MediaManager.get().url().transformation(new Transformation().effect("cartoonify")).generate(publicId);
+        ImageView transformComplexImageview = binding.transformComplexImageview;
+        Glide.with(this).load(URL).into(transformComplexImageview);
+
+
+
+
     }
 
 }
