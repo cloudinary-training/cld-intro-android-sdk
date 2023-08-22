4f18ec04981f3a84ad21440b0705ca858c4f2966
adimiz1 <adi.mizrahi@cloudinary.com>
2023-08-22 20:05:57 +0300

    Add upload button

--- app/src/main/java/com/cloudinary/academy_course/Fragments/UploadFragment.java
+++ app/src/main/java/com/cloudinary/academy_course/Fragments/UploadFragment.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
@@ -38,8 +39,17 @@ public class UploadFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        
-        uploadImage();
+        setUploadButton();
+    }
+
+    private void setUploadButton() {
+        Button uploadButton = binding.uploadButton;
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadImage();
+            }
+        });
     }
 
     private void uploadImage() {
