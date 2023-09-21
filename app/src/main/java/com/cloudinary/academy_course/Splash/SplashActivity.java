2a21b20312bc90edaa373727b02b7394e53bb8ef
adimiz1 <adi.mizrahi@cloudinary.com>
2023-09-21 11:47:08 +0300

    Add splash screen

---
+++ app/src/main/java/com/cloudinary/academy_course/Splash/SplashActivity.java
@@ -0,0 +1,45 @@
+package com.cloudinary.academy_course.Splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.cloudinary.academy_course.MainActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.navigation.ui.AppBarConfiguration;
+
+import com.cloudinary.academy_course.databinding.ActivitySplashBinding;
+import com.cloudinary.academy_course.utils.Utils;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivitySplashBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Utils.setStatusBarColor(this);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Intent intent = new Intent(this, MainActivity.class);
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+            }
+        }, 4000);
+    }
+
+
+
+}
