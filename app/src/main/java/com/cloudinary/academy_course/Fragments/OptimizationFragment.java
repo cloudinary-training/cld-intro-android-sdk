cf1081b98fcd20755e42f5ae8a7a78780d9bfe47
adimiz1 <adi.mizrahi@cloudinary.com>
2024-02-05 14:13:22 +0200

    Update optimization fragment

--- app/src/main/java/com/cloudinary/academy_course/Fragments/OptimizationFragment.java
+++ app/src/main/java/com/cloudinary/academy_course/Fragments/OptimizationFragment.java
@@ -1,7 +1,6 @@
 package com.cloudinary.academy_course.Fragments;
 
 import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,17 +8,14 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.target.CustomTarget;
 import com.bumptech.glide.request.target.SimpleTarget;
 import com.bumptech.glide.request.transition.Transition;
 import com.cloudinary.Transformation;
 import com.cloudinary.academy_course.Utils.Utils;
 import com.cloudinary.academy_course.databinding.OptimizationFragmentBinding;
-import com.cloudinary.academy_course.databinding.TransformFragmentBinding;
 import com.cloudinary.android.MediaManager;
 
 import java.io.ByteArrayOutputStream;
@@ -59,23 +55,19 @@ public class OptimizationFragment extends Fragment {
                 .into(new SimpleTarget<Bitmap>() {
                     @Override
                     public void onResourceReady(@NonNull Bitmap bitmap, Transition<? super Bitmap> transition) {
-                        int bytes = bitmap.getByteCount();
-                        double megabytes = bytes / (1024.0 * 1024.0);
 
-                        // Get the length of the bitmap's compressed data in bytes
                         ByteArrayOutputStream stream = new ByteArrayOutputStream();
                         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
                         byte[] byteArray = stream.toByteArray();
                         int fileLength = byteArray.length;
-
-                        // Now 'fileLength' represents the length of the image file in bytes
 
                         ImageView imageView = binding.optimizationOriginalImageview;
                         imageView.setImageBitmap(bitmap);
 
                         String imageDimensions = Utils.getImageWidhtAndHeightString(imageView.getDrawable());
                         String imageSize = Utils.getImageSize(fileLength);
-                        binding.optimizationOriginalTextview.setText(imageDimensions + " " + imageSize);
+                        binding.optimizationOriginalTextviewDimensions.setText(String.format("%s - ", imageDimensions));
+                        binding.optimizationOriginalTextviewSize.setText(imageSize);
                     }
                 });
     }
@@ -88,23 +80,18 @@ public class OptimizationFragment extends Fragment {
                 .into(new SimpleTarget<Bitmap>() {
                     @Override
                     public void onResourceReady(@NonNull Bitmap bitmap, Transition<? super Bitmap> transition) {
-                        int bytes = bitmap.getByteCount();
-                        double megabytes = bytes / (1024.0 * 1024.0);
-
-                        // Get the length of the bitmap's compressed data in bytes
                         ByteArrayOutputStream stream = new ByteArrayOutputStream();
                         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
                         byte[] byteArray = stream.toByteArray();
                         int fileLength = byteArray.length;
-
-                        // Now 'fileLength' represents the length of the image file in bytes
 
                         ImageView imageView = binding.optimizationOptimizedImageview;
                         imageView.setImageBitmap(bitmap);
 
                         String imageDimensions = Utils.getImageWidhtAndHeightString(imageView.getDrawable());
                         String imageSize = Utils.getImageSize(fileLength);
-                        binding.optimizationOptimizedTextview.setText(imageDimensions + " " + imageSize);
+                        binding.optimizationOptimizedTextviewDimensions.setText(String.format("%s - ", imageDimensions));
+                        binding.optimizationOptimizedTextviewSize.setText(imageSize);
                     }
                 });
     }
